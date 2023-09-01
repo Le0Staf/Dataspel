5efea1eef54429b4e76c4c5037fb00363d4a7b0f
Leo Staf <lest0823@storsjogymnasiet.se>
2023-09-01 11:15:08 +0200

    Initial commit

---
+++ Dataspel/Dataspel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dataspel
+{
+    class Dataspel 
+    {
+        public string spelarläge;
+        public string genre;
+        public string namn;
+
+        public Dataspel(string Aspelarläge, string Agenre, string Anamn)
+        {
+            spelarläge = Aspelarläge;
+            genre = Agenre;
+            namn = Anamn;
+        }  
+    }
+}
---
+++ Dataspel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Dataspel;
+
+namespace Dataspel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string search;
+
+            Dataspel fortnite = new Dataspel("Multiplayer", "Shooter", "Fortnite");
+            Dataspel ark = new Dataspel("Singleplayer", "Survival", "Ark");
+
+            Console.WriteLine("Search for game: ");
+            search = Console.ReadLine();
+
+            foreach (Dataspel spel in new Dataspel[] { fortnite, ark })
+            {
+                if (search == spel.namn)
+                {
+                    Console.WriteLine("\nNamn: " + spel.namn + "\nGenre: " + spel.genre + "\nSpelarläge: " + spel.spelarläge);
+                }
+                else if (search != spel.namn)
+                {
+                    Console.Write("");
+                }
+            }
+        }
+    }
+}
